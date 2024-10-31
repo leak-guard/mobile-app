7377b2fe675e5fa415740e3596ec2092614b04c9
Szymon Wieczorek <doszymona@gmail.com>
2024-10-31 12:23:22 +0100

    small fixes

--- lib/widgets/blurred_top_edge.dart
+++ lib/widgets/blurred_top_edge.dart
@@ -8,8 +8,8 @@ class BlurredTopEdge extends StatelessWidget {
   const BlurredTopEdge({
     required this.child,
     this.height = 50.0,
-    Key? key,
-  }) : super(key: key);
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
